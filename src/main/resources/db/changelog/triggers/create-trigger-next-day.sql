58707692f44051f8a6b6237c86929df7c3b25306
Sergei Aidinov <incoming34@yandex.ru>
2023-02-22 00:54:00 +0300

    Создал таблицу registered_players

--- src/main/resources/db/changelog/triggers/create-trigger-next-day.sql
+++ src/main/resources/db/changelog/triggers/create-trigger-next-day.sql
@@ -1,5 +1,5 @@
 -- liquibase formatted sql
--- changeset pcoates33:trigger-1 splitStatements:false
+-- changeset liquibase:c2ef62f8-f1f8-4dc7-a6f6-b75e8d2992b4 splitStatements:false
 
 -- delimiter $$
 
@@ -10,7 +10,7 @@ CREATE TRIGGER  next_day
 BEGIN
     IF NEW.reg_ends <= (CURRENT_DATE + INTERVAL 1 DAY) THEN
         SIGNAL SQLSTATE '45000'
-      SET MESSAGE_TEXT = 'An error occurred';
+      SET MESSAGE_TEXT = 'Регистрация должна заканчиваться не позднее дня создания турнира';
 
     END IF;
 END-- $$
