0f6b547bf55a58f878af1c923bd8eaab70e46dd6
Sergei Aidinov <incoming34@yandex.ru>
2023-02-22 04:12:25 +0300

    Сделал процедуру, меняющую статус при удалении игрока с турнира

--- src/main/resources/db/changelog/createTables/create-table-registered_players.sql
+++ src/main/resources/db/changelog/createTables/create-table-registered_players.sql
@@ -5,9 +5,9 @@ CREATE TABLE registered_players (
                                     ID bigint auto_increment primary key,
                                     player_id bigint not null,
                                     tour_id bigint not null,
-                                    status ENUM('registered', 'reserved'),
+                                    status ENUM('registered', 'reserved') not null,
                                     CONSTRAINT PLAYER_ID foreign key (player_id) references player(id) ON DELETE CASCADE,
-                                    CONSTRAINT TOUR_ID foreign key (tour_id) references tournament(id) ON DELETE CASCADE,
+                                    CONSTRAINT TOUR_ID foreign key (tour_id) references upcoming_tournament_data(tour_id) ON DELETE CASCADE,
                                     CONSTRAINT UNIQUE (player_id, tour_id)
 
 );
